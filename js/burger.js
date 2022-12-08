c904ae36ebec95fc8e65b9c17277770ad7cd9374
AlexYess <aalexesetov2541@gmail.com>
2022-12-09 00:52:46 +0300

    9.12 final?

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,7 @@
+if (document.querySelector('.burger')){
 document.querySelector('.burger').addEventListener('click', () => {
 	document.querySelector('.burger').classList.toggle('active');
 	document.querySelector('.nav-menu').classList.toggle('active');
 	document.querySelector('body').classList.toggle('lock');
 })
+}
